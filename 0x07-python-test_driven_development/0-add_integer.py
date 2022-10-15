d18cc8ec958c9b025557a4d503ca7454310e2fdb
yos <yosef.debela@ju.edu.com>
2022-10-15 22:29:34 +0300

    project on test deriven develpment

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-This is the addition module.
-it adds 2 integers
-a and b must be first casted
-"""
+# 0-add_integer.py
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """a and are integers
-    Returns an integer: the addition of a and b
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    if not (isinstance(a, int)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return (a + b)
+    return (int(a) + int(b))
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python3
-""" Here is a short script to 
-test max_integer([..])"""
+# 6-max_integer_test.py
+"""Unittests for max_integer([..])."""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-# A testcase is created by subclassing unittest.TestCase
+
 class TestMaxInteger(unittest.TestCase):
+    """Define unittests for max_integer([..])."""
 
-    # All tests you make must be passable by the function below
-    # The unittest module provides tools for constructing and running tests
-    # test_ means it's a method
-    def test_upper(self):
-        self.assertEqual(max_integer([6, 7, 8, 9]), 9)
+    def test_ordered_list(self):
+        """Test an ordered list of integers."""
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
 
-    def test_none(self):
-        self.assertEqual(max_integer([]), None)
+    def test_unordered_list(self):
+        """Test an unordered list of integers."""
+        unordered = [1, 2, 4, 3]
+        self.assertEqual(max_integer(unordered), 4)
 
-    def test_one(self):
-        self.assertEqual(max_integer([2]), 2)
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value."""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
 
-    def test_one_neg(self):
-        self.assertEqual(max_integer([-10]), -10)
+    def test_empty_list(self):
+        """Test an empty list."""
+        empty = []
+        self.assertEqual(max_integer(empty), None)
 
-    def test_neg(self):
-        self.assertEqual(max_integer([1, -2, -3, -4]), 1)
+    def test_one_element_list(self):
+        """Test a list with a single element."""
+        one_element = [7]
+        self.assertEqual(max_integer(one_element), 7)
 
-    def test_middle(self):
-        self.assertEqual(max_integer([1, 3, 8, 2, 6]), 8)
+    def test_floats(self):
+        """Test a list of floats."""
+        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(floats), 15.2)
+
+    def test_ints_and_floats(self):
+        """Test a list of ints and floats."""
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_string(self):
+        """Test a string."""
+        string = "Brennan"
+        self.assertEqual(max_integer(string), 'r')
+
+    def test_list_of_strings(self):
+        """Test a list of strings."""
+        strings = ["Brennan", "is", "my", "name"]
+        self.assertEqual(max_integer(strings), "name")
+
+    def test_empty_string(self):
+        """Test an empty string."""
+        self.assertEqual(max_integer(""), None)
 
 if __name__ == '__main__':
     unittest.main()
-
-# The crux of each test is a call to assertEqual() to check 
-# for an expected result; assertTrue() or assertFalse() to verify a condition; 
-# or assertRaises() to verify that a specific exception gets raised. 
-# These methods are used instead of the assert statement so 
-# the test runner can accumulate all test results and produce a report.
