cd4d7721fb7279afe19583e98040e923c724d736
yos <yosef.debela@ju.edu.com>
2022-10-25 20:24:35 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-from os import path
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""adds all arguments to a Python list and save them to file"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
-else:
-    obj_json_file = []
 
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
-
-save_to_json_file(obj_json_file, 'add_item.json')
+file_name = "add_item.json"
+if __name__ == "__main__":
+    try:
+        my_list = load_from_json_file(file_name)
+    except FileNotFoundError:
+        my_list = []
+    for i, arg in enumerate(sys.argv):
+        if i > 0:
+            my_list.append(arg)
+    save_to_json_file(my_list, file_name)
