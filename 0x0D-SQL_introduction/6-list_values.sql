4f2a7d7faf77fd319637f03c9e1a5abc3adac6cb
yos <yosef.debela@ju.edu.com>
2022-11-30 14:14:43 +0300

    chenges

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,5 @@
--- This script list all data in a table
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
 
-SELECT * FROM first_tables;
-
+SELECT * FROM first_table;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
--- This script display the number of records with id= 89
+-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
 
-SELECT COUNT (id) FROM first_table WHERE id = 89;
-
+SELECT COUNT(*) FROM first_table WHERE id=89;
