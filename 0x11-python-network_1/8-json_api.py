1e9b8125250e27c05138f42a5806fc1b8b945302
yos <yosef.debela@ju.edu.et>
2024-02-25 21:18:30 +0300

    api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(response.get("id"),
+                                    response.get("name")))
     except ValueError:
         print("Not a valid JSON")
